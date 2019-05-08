b8245e579aed22844f07a62f6215db84ddadf7de
generic Mac <genericmac@generics-MacBook-Pro.local>
2019-05-08 14:59:56 -0400

    cleaned up some more of the code, made a table object literal, added an interval timer to re-calculate the arrival time and minutes away every minute

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -12,58 +12,123 @@ messagingSenderId: "71632803599"
 // Create a variable to reference the database
 // var database = ...
 firebase.initializeApp(config);
-
 // Create a variable to reference the database
 var database = firebase.database();
-var rowCount = 0;
 
+var intervalId;
 
-function arrivalTimeCalculation(currentTimeMoment, minutesAway) {
-    var arrivalTimeMoment = moment(currentTimeMoment).add(minutesAway, 'minutes');
-    var arrivalTime = arrivalTimeMoment.format('HH:mm');
-    return arrivalTime;
-}
+/**************************************** */
+// Table Object
+var tbl = {
+    rowCount: 0,
 
+    getMinutesAway: function (firstTrainTime, frequency, currentTimeMoment) {
+        // calc minutes since first train time
+        var minSinceFirstTrainTime = this.getMinutesSinceFirstTrain(currentTimeMoment, firstTrainTime);
+        var minutesAway;
+        // two cases for minutes away
+        if (parseInt(frequency) > minSinceFirstTrainTime) {
+            // 1) no trains have arrived yet
+            minutesAway = parseInt(frequency) - minSinceFirstTrainTime;
+        } else {
+            // 2) at least one train has arrived already
+            minutesAway = parseInt(frequency) - (minSinceFirstTrainTime % parseInt(frequency));
+        }
 
-function minSinceFirstTrainCalculation(currentTimeMoment, firstTrainTime) {
-        
-    var currentTime = currentTimeMoment.format('HH:mm');  // convert it to a string
-    // var currentTime = moment().format('HH:mm');
-    console.log('current time is: ' + currentTime);
+        console.log("difference in time is: " + minSinceFirstTrainTime);
+        return minutesAway;
+    },
 
-    // want the difference in minutes between first train and now
-    var timeDiff = moment(currentTime, 'HH:mm').diff(moment(firstTrainTime, 'HH:mm'));
-    var duration = moment.duration(timeDiff);
-    var minSinceFirstTrainTime = duration.asMinutes();
+    getArrivalTime: function (currentTimeMoment, minutesAway) {
+        var arrivalTimeMoment = moment(currentTimeMoment).add(minutesAway, 'minutes');
+        var arrivalTime = arrivalTimeMoment.format('HH:mm');
+        return arrivalTime;
+    },
 
-    // if first train left yesterday do extra calc to handle the wrap in 24 hour time
-    if (minSinceFirstTrainTime < 0 ) {
-        // handle the wrap condition
-        minSinceFirstTrainTime = (24 * 60) + minSinceFirstTrainTime;
-    }
-    return minSinceFirstTrainTime;
-}
+    getMinutesSinceFirstTrain: function (currentTimeMoment, firstTrainTime) {
+        var currentTime = currentTimeMoment.format('HH:mm');  // convert it to a string
+        // var currentTime = moment().format('HH:mm');
+        console.log('current time is: ' + currentTime);
 
-function minutesAwayCalculation (frequency, minSinceFirstTrainTime) {
-    var minutesAway;
-    // ok two cases
-    if (parseInt(frequency) > minSinceFirstTrainTime) {
-        // 1) no trains have arrived yet
-        minutesAway = parseInt(frequency) - minSinceFirstTrainTime;
-    } else {
-        // 2) at least one train has arrived already
-        minutesAway = parseInt(frequency) - (minSinceFirstTrainTime % parseInt(frequency));
-    }
-    return minutesAway;
+        // want the difference in minutes between first train and now
+        var timeDiff = moment(currentTime, 'HH:mm').diff(moment(firstTrainTime, 'HH:mm'));
+        var duration = moment.duration(timeDiff);
+        var minSinceFirstTrainTime = duration.asMinutes();
+
+        // if first train left yesterday do extra calc to handle the wrap in 24 hour time
+        if (minSinceFirstTrainTime < 0 ) {
+            // handle the wrap condition
+            minSinceFirstTrainTime = (24 * 60) + minSinceFirstTrainTime;
+        }
+        return minSinceFirstTrainTime;
+    }, 
+
+    buildTableRow: function (row) {
+        var newRow = $("<tr>").append(
+            $("<th>").text(++tbl.rowCount),
+            $("<td>").text(row.name),
+            $("<td>").text(row.destination),
+            $("<td>").text(row.frequency),
+            $("<td>").text(row.arrivalTime),
+            $("<td>").text(row.minutesAway),
+        );
+        // Append the new row to the table
+        $("#schedule-table > tbody").append(newRow);
+    },
+
+}  // end tbl object
+
+//for each entry in the table
+function tblUpdate() {
+    database.ref().once("value", function(snapshot) {
+
+        // storing the snapshot.val() in a variable for convenience
+        var sv = snapshot.val();
+
+        // first remove any rows and return rowCount to zero
+        while (tbl.rowCount > 0) {
+            document.getElementById("schedule-table").deleteRow(tbl.rowCount--);
+        }
+
+        // create the tableRow object
+        var tableRow = {};
+        // walk through each item from the database and create a table row then display it
+        for (var key in sv) {
+            console.log("key: " + key);
+            var myObj = sv[key];
+            tableRow.frequency = myObj['frequency'];
+            tableRow.destination = myObj['destination'];
+            tableRow.name = myObj['name'];
+            var firstTrainTime = myObj['firstTrainTime'];
+
+            var currentTimeMoment = moment();  // get the current time in a moment object
+            tableRow.minutesAway = tbl.getMinutesAway(firstTrainTime, tableRow.frequency, currentTimeMoment);
+            tableRow.arrivalTime = tbl.getArrivalTime(currentTimeMoment, tableRow.minutesAway);
+
+            // Console.loging the train data
+            console.log('first train time: ' + firstTrainTime);
+            console.log('from getDatabase: ' + tableRow.name + " " + tableRow.destination + " " + tableRow.frequency + " " + tableRow.arrivalTime + " " + tableRow.minutesAway);
+            // build a new row
+            tbl.buildTableRow(tableRow);
+        }
+
+      });
 }
 
 $(document).ready(function() {
-
+    // start the 60 second timer to redo the table 
+    intervalId = setInterval(tblUpdate, 60000);
     // Capture Button Click
     $("#add-user").on("click", function(event) {
         // Don't refresh the page!
         event.preventDefault();
-  
+        
+        // make sure the timer doesn't fire until we get through here
+        // so stop and restart the interval timer
+        // 60 seconds from now should be plenty of time to get through updating table
+        clearInterval(intervalId);
+        intervalId = setInterval(tblUpdate, 60000);
+
         // Code in the logic for storing and retrieving the most recent user.
         var name = $('#name-input').val();
         var destination = $('#destination-input').val();
@@ -91,49 +156,27 @@ $(document).ready(function() {
     database.ref().on("child_added", function(snapshot) {
         // storing the snapshot.val() in a variable for convenience
         var sv = snapshot.val();
-
-        // Console.loging the last user's data
+        // Console.loging the train data
         console.log('from firebase name: ' + sv.name);
         console.log('destination: ' + sv.destination);
         console.log('first train time: ' + sv.firstTrainTime);
         console.log('frequency: ' + sv.frequency);
+        var tableRow = {};
+        tableRow.name = sv.name;
+        tableRow.destination = sv.destination;
+        tableRow.frequency = sv.frequency;
 
         // a moment is an object
         // a duration is just a number (ie. hours, minutes, etc..)
         // .format converts a moment to a string in a particular 'format' 
-
         var currentTimeMoment = moment();  // get the current time in a moment object
-        var minSinceFirstTrainTime = minSinceFirstTrainCalculation(currentTimeMoment, sv.firstTrainTime);
-        var minutesAway = minutesAwayCalculation(sv.frequency, minSinceFirstTrainTime);
-        console.log("difference in time is: " + minSinceFirstTrainTime);
-        console.log("minutes away " + minutesAway);
 
-        var arrivalTime = arrivalTimeCalculation(currentTimeMoment, minutesAway);
-        console.log("arrival time " + arrivalTime);
-        console.log(" ");
-        console.log(" ");
+        tableRow.minutesAway = tbl.getMinutesAway(sv.firstTrainTime, sv.frequency, currentTimeMoment);
+        tableRow.arrivalTime = tbl.getArrivalTime(currentTimeMoment, tableRow.minutesAway);
+
+        console.log(" "); // bjt delete this
+        tbl.buildTableRow(tableRow);
     
-        var newRow = $("<tr>").append(
-            $("<th>").text(++rowCount),
-            $("<td>").text(sv.name),
-            $("<td>").text(sv.destination),
-            $("<td>").text(sv.frequency),
-            $("<td>").text(arrivalTime),
-            $("<td>").text(minutesAway),
-        );
-        // Append the new row to the table
-        $("#schedule-table > tbody").append(newRow);
-
-
-        // udpate the most next train to arrive
-        if (parseInt(minutesAway) < parseInt($('#minutes-away-display').text())) {
-            $("#name-display").text(sv.name);
-            $("#destination-display").text(sv.destination);
-            $("#frequency-display").text(sv.frequency);
-            $("#next-arrival-display").text(arrivalTime);
-            $("#minutes-away-display").text(minutesAway);
-        }
-
         // Handle the errors
     }, function(errorObject) {
         console.log("Errors handled: " + errorObject.code);
